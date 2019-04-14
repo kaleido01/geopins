bd715a9c5b31c8c9771157c8bd8c6dfaf7f37192
kaleido <kaleido@gmail.com>
2019-04-14 21:29:21 +0900

    implement protectedRoute

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React,{ useContext,useReducer } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "mapbox-gl/dist/mapbox-gl.css";
+import * as serviceWorker from "./serviceWorker";
+
 
 import App from "./pages/App";
 import Splash from "./pages/Splash";
 import Context from "./context"
 import reducer from './reducer';
-import "mapbox-gl/dist/mapbox-gl.css";
-import * as serviceWorker from "./serviceWorker";
+import ProtectedRoute from "./ProtectedRoute"
+
 
 const Root = () => {
   const initialState=useContext(Context)
@@ -18,7 +21,7 @@ const Root = () => {
     <Router>
       <Context.Provider value={{state,dispatch}}>
       <Switch>
-        <Route exact path="/" component={App} />
+        <ProtectedRoute exact path="/" component={App} />
         <Route path="/login" component={Splash} />
       </Switch>
       </Context.Provider>
